d35d09f7149bc429e91a0ccc6692fae54087b7cd
dong-fa <120082854+dong-fa@users.noreply.github.com>
2025-07-09 15:21:10 +0900

    init commit
    init common

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,5 @@ export default function Home() {
   return;
   <>
     <h1>Final-Project-Sample</h1>
-    <div>
-      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-    </div>
   </>;
 }
