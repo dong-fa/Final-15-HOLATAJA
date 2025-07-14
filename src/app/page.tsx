6997794e191b5757cd249cd07ed35be21b9669a0
dong-fa <cdh0283@gmail.com>
2025-07-14 05:35:14 +0000

    style: tailwind 디자인시스템 세팅
    디자인 시스템 변수 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="title text-primary">Final-Project-Sample</h1>
-      <div className="w-100 h-100 bg-negative"></div>
-      <q className="label-l">kjahsdkahsdkjashdkajshduwhduwakjdhakuhwudakjhsfuwakjfhauwkhf</q>
-      <q className="label-m">kjahsdkahsdkjashdkajshduwhduwakjdhakuhwudakjhsfuwakjfhauwkhf</q>
-      <q className="label-s">kjahsdkahsdkjashdkajshduwhduwakjdhakuhwudakjhsfuwakjfhauwkhf</q>
-      <q className="contents">kjahsdkahsdkjashdkajshduwhduwakjdhakuhwudakjhsfuwakjfhauwkhf</q>
+      <h1 className="title">Final-Project-Sample</h1>
     </>
   );
 }
