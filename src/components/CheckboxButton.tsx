dd29b2223cd3f65cb59ba301834379e247ddb303
sugonhwang <sugonhwang1@gmail.com>
2025-07-15 17:34:51 +0900

    refactor: 체크박스 및 개인정보 이용 및 수집 약관 위치 수정

--- src/components/CheckboxButton.tsx
+++ src/components/CheckboxButton.tsx
@@ -7,18 +7,33 @@ type CheckboxButtonProps = {
 
 export default function CheckboxButton({ checked, onCheck }: CheckboxButtonProps) {
   return (
-    // flex로 감싸서 checkbox와 텍스트를 한 줄에 정렬
-    <label className="mt-5 flex items-center cursor-pointer">
-      {/* 
-        기본 checkbox 
-        - sr-only 제거 → 그대로 보임
-        - accent-color로 색상 지정
-        - w-5 h-5로 크기 지정
-      */}
-      <input type="checkbox" checked={checked} onChange={onCheck} className="w-5 h-5 " />
-
-      {/* 오른쪽 텍스트 */}
-      <span className="ml-2 text-[1rem] text-[var(--color-secondary)]">개인정보 수집 및 이용 약관에 동의합니다</span>
+    <label className="items-center cursor-pointer mt-5">
+      <input type="checkbox" checked={checked} onChange={onCheck} className="sr-only" />
+      <div
+        className={`
+      w-5 h-5
+      rounded
+      inline-block
+      border-2
+      transition-colors
+      
+      ${checked ? 'bg-[var(--color-primary)] border-[var(--color-primary)]' : 'bg-transparent border-[var(--color-gray)]'}
+    `}
+      >
+        {checked && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-3.5 h-3.5 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={3}
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+          </svg>
+        )}
+      </div>
+      <span className="ml-2 text-[1rem] text-[var(--color-secondary)] align-top">개인정보 수집 및 이용 약관에 동의합니다</span>
     </label>
   );
 }
