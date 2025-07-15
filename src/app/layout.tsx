777de98c374a357b42f3d69442aa1d5b4615d254
dong-fa <120082854+dong-fa@users.noreply.github.com>
2025-07-15 15:17:56 +0900

    style:layout 스타일 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className="min-h-screen flex flex-col">
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 border-b-1 border-background">{children}</main>
         <Footer />
       </body>
     </html>
