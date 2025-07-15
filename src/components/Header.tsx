80c392a331acd3788de7a18dad6b5673370cc005
dong-fa <120082854+dong-fa@users.noreply.github.com>
2025-07-15 14:41:52 +0900

    feat: icon 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="w-full flex justify-between items-center px-4 sub-title h-[60px] border-b-1 border-background">
       <div className="flex justify-center items-center gap-2">
-        <Image src="/logo.svg" alt="올라타자 로고" width={50} height={30} />
+        <Image src="/icon/logo.svg" alt="올라타자 로고" width={50} height={30} />
         <h1>HOLA TAJA!</h1>
       </div>
     </header>
