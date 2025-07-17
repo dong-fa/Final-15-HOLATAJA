570b5ab32a5905d63c0b0f2f3da10b9864cb6802
dong-fa <120082854+dong-fa@users.noreply.github.com>
2025-07-17 17:10:20 +0900

    style: 레이아웃 스타일 변경
    좌우 여백 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className="min-h-screen flex flex-col">
         <Header />
-        <main className="flex-1 border-b-1 border-background">{children}</main>
+        <main className="max-w-5xl px-4 sm:px-6 lg:px-8 w-full mx-auto flex-1 border-b-1 border-background">{children}</main>
         <Footer />
       </body>
     </html>
