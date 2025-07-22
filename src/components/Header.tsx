7359d2dec7512a23ada9769330a02e22c3353e44
hjb0304 <shany34@naver.com>
2025-07-22 13:29:16 +0900

    refactor: 마이페이지 href 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
           <h1 className="text-2xl">HOLA TAJA!</h1>
         </Link>
         <div className="flex gap-2">
-          <Link href="/mypage" title="마이페이지">
+          <Link href="/my" title="마이페이지">
             <User color={path === '/my' ? 'var(--color-primary)' : 'currentColor'} />
           </Link>
           <Link href="/cart" title="장바구니">
