f28c539cb04a2a9e4762fa469cef0a6349ab92dc
hjb0304 <shany34@naver.com>
2025-07-22 13:14:05 +0900

    style: 레이아웃 상하 패딩 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="ko" className="text-sm sm:text-base">
       <body className="flex flex-col min-h-screen bg-background">
         <Header />
-        <main className="flex-1 w-full max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">{children}</main>
+        <main className="flex-1 w-full max-w-5xl p-4 mx-auto sm:p-6 lg:p-8">{children}</main>
         <Footer />
       </body>
     </html>
