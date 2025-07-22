b39dcacd2536127ac7d3e84976be27894749d98c
hjb0304 <shany34@naver.com>
2025-07-22 10:42:12 +0900

    style: Header 반응형 UI 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="w-full flex justify-between items-center px-4 sub-title h-[60px] border-b-1 border-background">
-      <div className="flex justify-center items-center gap-2">
+    <header className="bg-white flex justify-between items-center sub-title h-[60px]">
+      <div className="w-full flex items-center gap-2 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <Image src="/icon/logo.svg" alt="올라타자 로고" width={50} height={30} />
-        <h1>HOLA TAJA!</h1>
+        <h1 className="text-2xl">HOLA TAJA!</h1>
       </div>
     </header>
   );
