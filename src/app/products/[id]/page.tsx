60d934945cbf484c1c38bb8f4b9fe69fcda1a5b6
hjb0304 <shany34@naver.com>
2025-07-23 15:55:31 +0900

    feat: 상품 상세 페이지 상품 상세 조회 기능 구현

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -14,104 +14,81 @@ import Textarea from '@/components/Textarea';
 import { Contents, ContentsTitle, SubTitle, Title } from '@/components/Typography';
 import useAxiosInstance from '@/hooks/useAxiosInstance';
 import { KeyRound, Star } from 'lucide-react';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
-// 상품 상세 데이터 reponse
+// 상품 상세 reponse
 interface DataResProps {
   ok: number;
   item: DataProps;
 }
 
-// 상품 상세 데이터 item
+// 상품 상세 item
 interface DataProps {
+  _id: number;
+  price: number;
+  quantity: number;
   name: string;
-  price: string;
-}
-
-// 상품 상세 정보
-const data = {
-  _id: 1,
-  seller_id: 2,
-  price: 219000,
-  shippingFees: 3000,
-  show: true,
-  active: true,
-  name: 'NUPHY AIR96 V2 기계식 키보드 청축 블루투스 무선 유선 풀배열 슬림형',
-  quantity: 300,
-  buyQuantity: 12,
-  mainImages: [
-    {
-      path: 'files/febc13-final15-emjf/nuphy_air96_lunagray_detail_01.webp',
-      name: 'nuphy_air96_lunagray_detail_01.webp',
-      originalname: 'AIR96',
-    },
-    {
-      path: 'files/febc13-final15-emjf/nuphy_air96_lunagray_detail_02.webp',
-      name: 'nuphy_air96_lunagray_detail_02.webp',
-      originalname: 'AIR96',
-    },
-    {
-      path: 'files/febc13-final15-emjf/nuphy_air96_lunagray_detail_03.webp',
-      name: 'nuphy_air96_lunagray_detail_03.webp',
-      originalname: 'AIR96',
-    },
-  ],
-  createdAt: '2025.07.10 15:07:54',
-  updatedAt: '2025.07.12 01:07:54',
+  content: string;
+  show: boolean;
+  active: boolean;
+  mainImages: {
+    type: 'detail' | 'info';
+    originalname: string;
+    name: string;
+    path: string;
+  }[];
   extra: {
-    isNew: false,
-  },
+    isNew: boolean;
+    category: 'BLUE' | 'BROWN' | 'RED' | 'OTHER';
+    option: string[];
+    description: string;
+    'function-tag': string[];
+    'soundfile-path': string;
+  };
+  shippingFees: number;
+  seller_id: number;
+  buyQuantity: number;
+  createdAt: string;
+  updatedAt: string;
   seller: {
-    _id: 2,
-    email: 's1@market.com',
-    name: '네오',
-    phone: '01022223333',
-    address: '서울시 강남구 삼성동 456',
-    image: '/files/febc13-final15-emjf/user-neo.png',
+    _id: number;
+    email: string;
+    name: string;
+    phone: string;
+    address: string;
     extra: {
-      birthday: '11-23',
-    },
-  },
-  replies: [
-    {
-      _id: 7,
-      rating: 1,
-      content: '키보드에 커피를 쏟았어요',
-      user_id: 5,
-      user: {
-        _id: 5,
-        name: '희정',
-        image: null,
-      },
-      createdAt: '2025.07.17 11:16:18',
-    },
-    {
-      _id: 8,
-      rating: 5,
-      content: '강아지가 좋아해요',
-      user_id: 5,
-      user: {
-        _id: 5,
-        name: '희정',
-        image: null,
-      },
-      createdAt: '2025.07.17 11:47:50',
-    },
-  ],
-  rating: 3,
-  bookmarks: 1,
-  options: [],
-};
+      birthday: string;
+      address: { id: number; name: string; value: string }[];
+    };
+  };
+  replies: {
+    _id: number;
+    rating: number;
+    content: string;
+    user_id: number;
+    user: {
+      _id: number;
+      name: string;
+      image: null;
+    };
+    createdAt: '2025.07.17 11:16:18';
+  }[];
+  bookmarks: number;
+  options: [];
+}
 
 // 상품 구매 시 전달할 정보
 const purchaseData = [];
 
 function ProductInfo() {
-  // const [data, setData] = useState<DataProps | null>(null);
-  const [quantity, setQuantity] = useState(0);
-  const [selectedValue, setSelectedValue] = useState();
-
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const params = useParams().id;
   const axios = useAxiosInstance();
+
+  const [data, setData] = useState<DataProps | null>(null);
+  const [quantity, setQuantity] = useState(0);
 
   const tabItems: TabItem[] = [
     {
@@ -121,30 +98,28 @@ function ProductInfo() {
         <div className="flex flex-col gap-6 px-4 sm:gap-12">
           <div>
             <ContentsTitle className="mb-4">제품 소개</ContentsTitle>
-            <p>제품 소개글</p>
+            <p>{data?.extra.description}</p>
           </div>
           <div>
             <ContentsTitle className="mb-4">기능</ContentsTitle>
             <ul className="grid gap-2 sm:gap-4 sm:grid-cols-3">
-              <li className="flex items-center justify-center h-16 gap-2 p-4 font-semibold bg-white border rounded-lg border-lightgray">
-                <KeyRound />
-                <div>Hot-Swappable Switches</div>
-              </li>
-              <li className="flex items-center justify-center h-16 gap-2 p-4 font-semibold bg-white border rounded-lg border-lightgray">
-                <KeyRound />
-                Customizable RGB Lighting
-              </li>
-              <li className="flex items-center justify-center h-16 gap-2 p-4 font-semibold bg-white border rounded-lg border-lightgray">
-                <KeyRound />
-                Compact 75% Layout
-              </li>
+              {/* {data?.extra['function-tag'].map((tag, idx) => (
+                <li key={idx} className="flex items-center justify-center h-16 gap-2 p-4 font-semibold bg-white border rounded-lg border-lightgray">
+                  <KeyRound />
+                  <div>tag</div>
+                </li>
+              ))} */}
             </ul>
           </div>
           <div className="rounded-lg bg-lightgray">
             <SoundToggle />
             <div>{/* 키보드 */}</div>
           </div>
-          <div>{/* 상세 이미지 */}</div>
+          {(data?.mainImages.filter(img => img.type === 'info').map(img => API_URL + '/' + img.path) ?? []).map((img, idx) => (
+            <div key={idx} className="relative w-full">
+              <Image src={img} alt="" width={0} height={0} priority sizes="100%" className="w-full h-auto object-scale-down" />
+            </div>
+          ))}
         </div>
       ),
     },
@@ -154,31 +129,17 @@ function ProductInfo() {
       content: (
         <>
           <div className="flex flex-col gap-4 mb-6 sm:mb-12">
-            {' '}
-            <ReviewCard
-              name="박동건"
-              createdAt="2025.03.14 20:07:54"
-              rating={4}
-              content="처음으로 num 키 없는 키보드를 사봤는데요, num키가 문제가 아니라 이 키보드 방향키가 왜 이모양이죠???? 
+            {data?.replies.map((reply, idx) => (
+              <ReviewCard
+                key={idx}
+                name="박동건"
+                createdAt="2025.03.14 20:07:54"
+                rating={4}
+                content="처음으로 num 키 없는 키보드를 사봤는데요, num키가 문제가 아니라 이 키보드 방향키가 왜 이모양이죠???? 
 크아하다가 역주행 8번 했네요.
 아 잘 보고 살걸... 제가 마음이 여린 편이라 별점은 4점 드립니다..."
-            ></ReviewCard>
-            <ReviewCard
-              name="박동건"
-              createdAt="2025.03.14 20:07:54"
-              rating={2}
-              content="처음으로 num 키 없는 키보드를 사봤는데요, num키가 문제가 아니라 이 키보드 방향키가 왜 이모양이죠???? 
-크아하다가 역주행 8번 했네요.
-아 잘 보고 살걸... 제가 마음이 여린 편이라 별점은 4점 드립니다..."
-            ></ReviewCard>
-            <ReviewCard
-              name="박동건"
-              createdAt="2025.03.14 20:07:54"
-              rating={1}
-              content="처음으로 num 키 없는 키보드를 사봤는데요, num키가 문제가 아니라 이 키보드 방향키가 왜 이모양이죠???? 
-크아하다가 역주행 8번 했네요.
-아 잘 보고 살걸... 제가 마음이 여린 편이라 별점은 4점 드립니다..."
-            ></ReviewCard>
+              ></ReviewCard>
+            ))}
           </div>
           <div className="flex flex-col gap-4">
             <Contents size="large">구매 후기 등록하기</Contents>
@@ -217,20 +178,35 @@ function ProductInfo() {
     },
   ];
 
+  // 상품 상세 조회
+  const getData = async () => {
+    try {
+      const response = await axios.get<DataResProps>(`/products/${params}`);
+      setData(response.data.item);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 상품 구매 후기 목록 조회
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="flex flex-col gap-6 sm:gap-12">
       <Title>상품 상세</Title>
       <div className="grid sm:grid-cols-2 gap-9">
         <ProductImg
           title={data?.name ?? ''}
-          srcList={['/product_images/nuphy_halo75/nuphy_halo75_detail_01.webp', '/product_images/nuphy_halo75/nuphy_halo75_detail_02.webp']}
-          // srcList={data.mainImages.map(img => img.path)}
+          srcList={data?.mainImages.filter(img => img.type === 'detail').map(img => API_URL + '/' + img.path) ?? []}
           swipe
         ></ProductImg>
         <div className="flex flex-col self-center gap-4">
           <div>
             <SubTitle className="mb-2">{data?.name ?? ''}</SubTitle>
-            <p className="text-secondary">상품 설명</p>
+            <p className="text-secondary">{data?.content}</p>
           </div>
           <div>
             <span className="text-2xl font-bold">{data?.price.toLocaleString()}원</span>
@@ -238,19 +214,15 @@ function ProductInfo() {
           <div>
             <span className="inline-block mb-2 font-semibold">옵션</span>
             <div className="flex gap-2">
-              <Button size="medium" select>
-                옵션1
-              </Button>
-              <Button size="medium" select>
-                옵션1
-              </Button>
-              <Button size="medium" select>
-                옵션1
-              </Button>
+              {data?.extra.option.map((option, idx) => (
+                <Button key={idx} size="medium" select>
+                  {option}
+                </Button>
+              ))}
             </div>
           </div>
           <div className="flex flex-col gap-4">
-            <QuantityCount handleCountQuantity={num => setQuantity(num)} quantity={0} />
+            <QuantityCount handleCountQuantity={num => setQuantity(num)} quantity={quantity} />
             <div className="flex flex-row gap-2 sm:gap-4">
               <Button outlined size="full">
                 장바구니
