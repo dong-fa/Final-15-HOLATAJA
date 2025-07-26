4664da12f3b139bfa55a0889b5eaf62c833757db
hjb0304 <shany34@naver.com>
2025-07-26 11:09:37 +0900

    feat: 구매 후기 카드 컴포넌트 내 구매 후기 여부, 삭제 함수 속성 추가

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -8,9 +8,11 @@ interface ReviewCardProps {
   createdAt: string;
   rating: number;
   content: string;
+  isMyReview?: boolean;
+  handleDelete: () => void;
 }
 
-function ReviewCard({ name, createdAt, rating, content }: ReviewCardProps) {
+function ReviewCard({ name, createdAt, rating, content, isMyReview, handleDelete }: ReviewCardProps) {
   // 별점 반환
   const stars = () => {
     const starsList = [];
@@ -27,29 +29,33 @@ function ReviewCard({ name, createdAt, rating, content }: ReviewCardProps) {
   };
 
   return (
-    <div className="flex items-center w-full py-4 border-b-1 border-b-disabled">
-      <div className="flex flex-col w-full gap-2">
-        <div className="flex flex-col">
-          <span>{name}</span>
-          <span className="label-s text-secondary">{createdAt.split(' ')[0]}</span>
+    <>
+      <div className="flex items-center w-full py-4 border-b-1 border-b-disabled">
+        <div className="flex flex-col w-full gap-2">
+          <div className="flex flex-col">
+            <span>{name}</span>
+            <span className="label-s text-secondary">{createdAt.split(' ')[0]}</span>
+          </div>
+          <div className="flex gap-0.5">
+            <span className="sr-only">{`별점 ${rating}/5점`}</span>
+            {stars()}
+          </div>
+          <div>
+            <p>{content}</p>
+          </div>
         </div>
-        <div className="flex gap-0.5">
-          <span className="sr-only">{`별점 ${rating}/5점`}</span>
-          {stars()}
-        </div>
-        <div>
-          <p>{content}</p>
-        </div>
+        {isMyReview && (
+          <div className="flex">
+            <Button icon size="small" aria-label="수정">
+              <Pencil color="var(--color-darkgray)" size={20} />
+            </Button>
+            <Button icon size="small" aria-label="삭제">
+              <Trash color="var(--color-darkgray)" size={20} onClick={handleDelete} />
+            </Button>
+          </div>
+        )}
       </div>
-      <div className="flex">
-        <Button icon size="small" aria-label="수정">
-          <Pencil color="var(--color-darkgray)" size={20} />
-        </Button>
-        <Button icon size="small" aria-label="삭제">
-          <Trash color="var(--color-darkgray)" size={20} />
-        </Button>
-      </div>
-    </div>
+    </>
   );
 }
 
