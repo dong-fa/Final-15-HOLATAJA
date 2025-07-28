42ec3454233d3a006347dc6c230b527c585684e7
whynotKeys <wildcat0426@naver.com>
2025-07-29 00:29:37 +0900

    fix: props 포함해서 getOrderList() 사용하도록 수정

--- src/app/my/components/OrderTab.tsx
+++ src/app/my/components/OrderTab.tsx
@@ -1,23 +1,25 @@
 import HistoryCard from '@/app/my/components/HistoryCard';
-import Pagination from '@/components/Pagination';
 import { SubTitle } from '@/components/Typography';
 import { getOrderList } from '@/data/functions/order';
 import { getOrderStatusLabel } from '@/data/tables/mappingTables';
+import { OrderItem } from '@/types/order';
 
 export default async function OrderTab() {
-  const orderHistoryData = await getOrderList();
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  const orderHistoryData = await getOrderList({ page: 1, limit: 3 });
   const orderHistoryList = orderHistoryData.ok === 1 ? orderHistoryData.item : [];
 
   return (
     <>
       <SubTitle className="label-l">구매 내역</SubTitle>
       <div className="bg-white py-3 mt-3">
-        {orderHistoryList.map((order, index) => (
+        {orderHistoryList.map((order: OrderItem, index: number) => (
           <HistoryCard
             key={index}
             id={order._id}
             status={getOrderStatusLabel(order.state)}
-            src={`https://fesp-api.koyeb.app/market/${order.products[0].image.path}`}
+            src={`${API_URL}/${order.products[0].image.path}`}
             name={order.products[0].name}
             price={order.cost.total}
             quantity={order.products.length}
@@ -25,7 +27,6 @@ export default async function OrderTab() {
           />
         ))}
       </div>
-      <Pagination totalPages={3} currentPage={1} />
     </>
   );
 }
