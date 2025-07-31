cdc10e9b075cb65c5fd30efb7cdbd553af5f489b
hjb0304 <shany34@naver.com>
2025-07-31 21:35:59 +0900

    feat: Q&A 수정 기능 구현

--- src/data/actions/qna.ts
+++ src/data/actions/qna.ts
@@ -47,7 +47,7 @@ export async function postQuestion(state: ApiRes<QuestionItem> | null, formData:
 /**
  * Q&A id에 해당하는 Q&A를 삭제함
  * @param {number} _id - Q&A id
- * @returns {Promise<ApiRes>} - Q&A id에 해당하는 Q&A 응답 객체
+ * @returns {Promise<ApiRes<QuestionItem>>} - Q&A id에 해당하는 Q&A 응답 객체
  */
 export async function deleteQnA(_id: number): ApiResPromise<QuestionItem> {
   let data: ApiRes<QuestionItem>;
@@ -65,6 +65,37 @@ export async function deleteQnA(_id: number): ApiResPromise<QuestionItem> {
   } catch (error) {
     console.error(error);
     return { ok: 0, message: 'Q&A 삭제에 실패했습니다.' };
+  }
+
+  if (data.ok) {
+    revalidateTag('qna-list');
+    revalidateTag('my-qna-list');
+  }
+  return data;
+}
+
+/**
+ * Q&A id에 해당하는 질문 게시글을 입력된 제목과 내용으로 수정함
+ * @param {number} _id - 수정할 Q&A id
+ * @param {string} title - 수정된 Q&A 제목
+ * @param {string} content: 수정된 Q&A 내용
+ * @returns {Promise<ApiRes<QuestionItem>>}
+ */
+export async function patchQnA(_id: number, title: string, content: string): ApiResPromise<QuestionItem> {
+  let data: ApiRes<QuestionItem>;
+  const accessToken = (await cookies()).get('accessToken')?.value;
+  const body = { _id, title, content };
+
+  try {
+    const response = await fetch(`${API_URL}/posts/${_id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json', 'Client-Id': CLIENT_ID, Authorization: `Bearer ${accessToken}` },
+      body: JSON.stringify(body),
+    });
+    data = await response.json();
+  } catch (error) {
+    console.error(error);
+    return { ok: 0, message: 'Q&A 수정에 실패했습니다.' };
   }
 
   if (data.ok) {
