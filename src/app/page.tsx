e403c974110ad50c9ddc23e1b68c7381b554c66c
whynotKeys <wildcat0426@naver.com>
2025-08-04 13:11:25 +0900

    fix: 메인 페이지에 products/로 리다이렉션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <>
-      <h1 className="title">Final-Project-Sample</h1>
-    </>
-  );
+  redirect('/products');
 }
