bdc3c5fb2c0a059e7bba4d55e262b28e9382ec67
dong-fa <120082854+dong-fa@users.noreply.github.com>
2025-08-04 23:13:43 +0900

    feat: 파비콘 추가
    라이트 모드 버전
    다크 모드 버전
    레이아웃에 메타데이터로 추가함
    
    #197

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,27 @@
-import Footer from '@/components/Footer';
 import './globals.css';
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import type { Metadata } from 'next';
 // import { SessionProvider } from 'next-auth/react';
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      rel: 'icon',
+      url: '/icon/favicon_light.svg',
+      media: '(prefers-color-scheme: light)',
+    },
+    {
+      rel: 'icon',
+      url: '/icon/favicon_dark.svg',
+      media: '(prefers-color-scheme: dark)',
+    },
+    {
+      rel: 'icon',
+      url: '/icon/favicon_light.svg',
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
